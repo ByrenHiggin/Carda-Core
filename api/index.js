195775c24db51419c238d6c8d5ca2935cacad183
Byren Higgin <byrenhiggin1994@hotmail.com>
2018-10-18 09:44:07 +1100

    Add Initial API example
    Added a rough task api management system

---
+++ api/index.js
@@ -0,0 +1,30 @@
+module.exports = function(){
+    'use strict';
+    const mongoose = require('mongoose');
+    const express = require('express');
+    const Task = require('./__start__');
+    const bodyParser = require('body-parser');
+    const app = express()
+    const port = process.env.API_PORT || 8081;
+
+    
+    mongoose.Promise = global.Promise;
+    console.log(process.env.MDBASE);
+    mongoose.connect(process.env.MDBASE);
+    
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(bodyParser.json());
+
+    var routes = require('./routes/routes'); //importing route
+    routes(app);
+    
+    app.use(function(req, res) {
+      res.status(404).send({url: req.originalUrl + ' not found'})
+    });
+    console.log('todo list RESTful API server started on: ' + port);
+
+    
+    app.listen(port);
+
+    
+}();
---
+++ server.js
@@ -0,0 +1,11 @@
+const api = require('./api/');
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+var app = express()
+var port = process.env.MAIN_PORT || 8080;
+
+app.listen(port);
+
+console.log('todo list RESTful API server started on: ' + port);
